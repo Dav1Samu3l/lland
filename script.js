e8f96d4b17052b1c22019ea8ff69aa50546c102c
Dav1Samu3l <daviflorianopolis@gmail.com>
2025-04-01 14:52:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let index = 0;
+function moveCarousel(direction) {
+    const carousel = document.getElementById('carousel');
+    const cards = document.querySelectorAll('.card');
+    const cardWidth = cards[0].offsetWidth + 20;
+    const maxIndex = cards.length - 3; 
+    
+    index += direction;
+    if (index < 0) index = 0;
+    if (index > maxIndex) index = maxIndex;
+    
+    carousel.style.transform = `translateX(${-index * cardWidth}px)`;
+}
