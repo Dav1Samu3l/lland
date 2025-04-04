65b3016d1c22ea22351b7efc2ef8fd057788071c
Dav1Samu3l <daviflorianopolis@gmail.com>
2025-04-04 17:55:49 -0300

    funçao de movimento para 2 Carrosséis

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let index = 0;
 function moveCarousel(direction) {
     const carousel = document.getElementById('carousel');
+    const carousel1 = document.getElementById('carousel1');
+
     const cards = document.querySelectorAll('.card');
     const cardWidth = cards[0].offsetWidth + 20;
-    const maxIndex = cards.length - 3; 
-    
+    const maxIndex = cards.length - 3;
+
     index += direction;
     if (index < 0) index = 0;
     if (index > maxIndex) index = maxIndex;
-    
+
     carousel.style.transform = `translateX(${-index * cardWidth}px)`;
+    carousel1.style.transform = `translateX(${-index * cardWidth}px)`;
+
+
 }
